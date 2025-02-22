e6daaca3ce5ce5403040390150fd01f2f2c07506
tonyekim <otoobongekim@gmail.com>
2025-02-22 21:23:17 +0100

    add cloudinary ai and google auto tagging

--- app/(root)/transformation/add/[type]/page.tsx
+++ app/(root)/transformation/add/[type]/page.tsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import Header from "@/app/components/shared/Header";
+import { transformationTypes } from "@/constants";
+import TransformationForm from "@/app/components/shared/TransformationForm";
+import { auth } from "@clerk/nextjs";
+import { getUserById } from "@/lib/actions/user.actions";
+import { redirect } from "next/navigation";
 
-const AddTransformationTypePage = () => {
+const AddTransformationTypePage = async ({
+  params: { type },
+}: SearchParamProps) => {
+  const { userId } = auth();
+
+  if (!userId) redirect("/sign-in");
+  const user = await getUserById(userId);
+
+  console.log(user);
+  const transformation = transformationTypes[type];
   return (
-    <div>AddTransformationTypePage</div>
-  )
-}
+    <>
+      <Header title={transformation.title} subtitle={transformation.subTitle} />
 
-export default AddTransformationTypePage
+      <section className="mt-10">
+        <TransformationForm
+          action="Add"
+          userId={user._id}
+          type={transformation.type as TransformationTypeKey}
+          creditBalance={user.creditBalance}
+        />
+      </section>
+    </>
+  );
+};
+
+export default AddTransformationTypePage;
